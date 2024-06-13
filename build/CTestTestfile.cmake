adb196f2c47f0d18030a989219d94e0fb6b3af03
qung11 <quangngo824@gmai.com>
2024-06-13 14:08:57 +0700

    wip

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /home/jetson/catkin_ws/src
+# Build directory: /home/jetson/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("process_marker_rqt")
+subdirs("yahboomcar_bringup")
+subdirs("yahboomcar_ctrl")
+subdirs("yahboomcar_description")
+subdirs("yahboomcar_laser")
+subdirs("yahboomcar_msgs")
+subdirs("yahboomcar_nav")
